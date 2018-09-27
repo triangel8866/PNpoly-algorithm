4816cf74117e94ccba863a797c63bcd2d32777c2
Yue <yuanyue8866@gmail.com>
2018-09-27 10:06:40 +0800

    Generate polygon tuple

--- PNpoly.py
+++ PNpoly.py
@@ -1,28 +1,31 @@
+#polygon algorithm
 def raycasting(p,poly):
+    #for each point; p is port location, poly is eca areas
     flag = False
-    for i in range(0,(len(poly) - 1)):
+    polygon = tuple(poly[:])+(poly[0],)
+    for i in range(0,(len(polygon) - 1)):
         j = i + 1
-        sx = poly['Latitude'][i]
-        sy = poly['Longitude'][i]
-        tx = poly['Latitude'][j]
-        ty = poly['Longitude'][j]
+        sx = polygon[i]['Latitude']
+        sy = polygon[i]['Longitude']
+        tx = polygon[j]['Latitude']
+        ty = polygon[j]['Longitude']
 
         x = np.NaN
         #points within two poly points y axis
-        if ((p.Longitude < ty and p.Longitude > sy) or (p.Longitude < sy and p.Longitude > ty)):
+        if ((p[1] < ty and p[1] > sy) or (p[1] < sy and p[1] > ty)):
             #points on straight line
-            x = sx + (ty - sy)*(ty - sy)/(tx - sy)
+            x = sx + (p[1] - sy)*(tx - sx)/(ty - sy)
 
-            #points on the line
-        if x == p.Latitude:
+        #points on the line
+        if x == p[0]:
             flag = True
 
         #raycasting line
-        if x > p.Latitude:
+        if x > p[0]:
             flag = not flag
 
         #polygon points concide with ports location
-        if ((sx == p.Latitude and sy == p.Longitude) or (tx == p.Latitude and ty == p.Longitude)):
+        if ((sx == p[0] and sy == p[1]) or (tx == p[0] and ty == p[1])):
             flag = True
 
     return flag
